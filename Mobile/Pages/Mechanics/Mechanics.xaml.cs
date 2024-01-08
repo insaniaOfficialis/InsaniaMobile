0bda2784315a77a83399e2b9c9b139419b2c6bbe
insaniaOfficialis <insania_officialis@vk.com>
2024-01-08 15:29:37 +0300

    Начата работа над плашками страницы механик

--- Mobile/Pages/Mechanics/Mechanics.xaml.cs
+++ Mobile/Pages/Mechanics/Mechanics.xaml.cs
@@ -1,4 +1,5 @@
 using Mobile.Pages.Base.Menu;
+using System.Collections.ObjectModel;
 
 namespace Mobile.Pages.Mechanics;
 
@@ -7,13 +8,15 @@ namespace Mobile.Pages.Mechanics;
 /// </summary>
 public partial class Mechanics : ContentPage
 {
-	/// <summary>
-	/// Конструктор класса страницы механик
-	/// </summary>
-	public Mechanics()
-	{
-		//Инициализируем компоненты
-		InitializeComponent();
+    private readonly ObservableCollection<Section> _sections;
+
+    /// <summary>
+    /// Конструктор класса страницы механик
+    /// </summary>
+    public Mechanics()
+    {
+        //Инициализируем компоненты
+        InitializeComponent();
 
         //Если мобильное устройство
         if (DeviceInfo.Idiom == DeviceIdiom.Phone)
@@ -35,6 +38,13 @@ public partial class Mechanics : ContentPage
             BottomMenuMobile bottomMenu = new("mechanics");
             bottomMenuContent.Children.Add(bottomMenu);
             Page.Add(bottomMenu, 0, 2);
+
+            Sections.ItemsLayout = new GridItemsLayout(ItemsLayoutOrientation.Vertical)
+            {
+                HorizontalItemSpacing = 10,
+                VerticalItemSpacing = 10,
+                Span = 2
+            };
         }
 
         //Если пк
@@ -46,7 +56,25 @@ public partial class Mechanics : ContentPage
                 Padding = new Thickness(0, 10, 0, 0)
             };
             Content.Children.Add(menu);
+
+            Sections.ItemsLayout = new GridItemsLayout(ItemsLayoutOrientation.Vertical)
+            {
+                HorizontalItemSpacing = 10,
+                VerticalItemSpacing = 10,
+                Span = 4
+            };
         }
+
+        Application.Current!.Resources.TryGetValue("SecondaryBackground", out var color);
+        Color background = (Color)color;
+        _sections =
+        [
+            new("system.png", "Системное", "Управление\nприложением", background),
+            new("general.png", "Общее", "Общеигровое\nвзаимодействие", background),
+            new("politic.png", "Политика", "Взаимодействие\nорганизаций", background),
+            new("sociology.png", "Социология", "Взаимодействие c\nобществом", background),
+        ];
+        Sections.ItemsSource = _sections;
     }
 
     /// <summary>
@@ -56,5 +84,41 @@ public partial class Mechanics : ContentPage
     protected override bool OnBackButtonPressed()
     {
         return true;
+    }
+
+    private class Section(string file, string title, string description, Color color)
+    {
+        public string File { get; private set; } = file;
+
+        public string Title { get; private set; } = title;
+
+        public string Description { get; private set; } = description;
+
+        public Color Color { get; set; } = color;
+    }
+
+    private void Sections_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        Application.Current!.Resources.TryGetValue("PrimaryBackground", out var color);
+        Color background = (Color)color;
+
+        if (e.CurrentSelection.FirstOrDefault() is Section current)
+        {
+            var element = _sections.First(x => x.Title == current.Title);
+            Section section = new(element.File, element.Title, element.Description, background);
+            var index = _sections.IndexOf(element);
+            _sections[index] = section;
+        }
+
+        Application.Current!.Resources.TryGetValue("SecondaryBackground", out color);
+        background = (Color)color;
+
+        if (e.PreviousSelection.FirstOrDefault() is Section previous)
+        {
+            var element = _sections.First(x => x.Title == previous.Title);
+            Section section = new(element.File, element.Title, element.Description, background);
+            var index = _sections.IndexOf(element);
+            _sections[index] = section;
+        }
     }
 }
